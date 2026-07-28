cd55fc5b0f9fdee1c5593a7965befa8002171b8e
paruru-05 <haruto.soe0524@gmail.com>
2026-07-28 17:23:26 +0900

    7/28 交差点クリアъ(`･ω･´)ｸﾞｯ

--- Test_MCP.py
+++ Test_MCP.py
@@ -1,7 +1,6 @@
 print("Hello!")
 from machine import Pin, SPI
 import utime
-from bno08x import *
 import mcp3008
 import rp2
 import omni
@@ -9,10 +8,9 @@ from MotorConfig import *
 from hcsr04 import HCSR04
 
 # CSピンをSPI0のペリフェラル領域外(例: Pin 15)に変更
-cs_pin = Pin(1, Pin.OUT)
-
 # baudrateを1MHz(1000000)に設定
 spi = SPI(0, sck=Pin(2), mosi=Pin(3), miso=Pin(4), polarity=0, phase=0, baudrate=1000000)
+cs_pin = Pin(1, Pin.OUT)
 
 # クラスに渡す
 mcp = mcp3008.mcp(spi, cs_pin)
@@ -36,37 +34,9 @@ def p(v, l=20, c=""):
     f = int(n * l)
     print(f"\r[{'|'*f}{' '*(l-f)}] {int(n*100)}% ({v})", end=f"{"    !!LINE!!    " if v < shikii_line else ""}{c}")
 
-o = omni.Omni(PWM_LIST,IN_2_LIST,IN_1_LIST,True)
-shikii_dis = 10
-dist = 0
-shikii_line = 150
-spd = 30000
-senkai = 1
-mae = 1.2
-yoko = 0
-check = False
-waitime = 500
-kst_rig = 0
-kst_lft = 0
-kstmv = -1 #交差点方向 -1左　1右
-kstsnk = 1.3 #交差点旋回速度
-ksty = 0.5 #交差点Vy値
-kstime = 800 #交差点曲がり時間
-kstzzny = 1 #交差点事前前進Vy値
-kszzntime = 1200 #交差点事前前進時間
-kst = 200 #交差点判定のはば
-kst_tik = 200 #交差点後の待機
-lost = False
-lostime = 500
-losted = 0
-latest = None
-hcsr.activate()
-
-sleep_ms(50)
-
 def normalTrace():
     global lost, latestmove, losted
-    if mcp.read_adc(1) < shikii_line or mcp.read_adc(0) < shikii_line:
+    if mcp.read_adc(1) < shikii_line:
         o.move(-yoko,mae,spd,-senkai)
         lost = False
         latestmove = "r"
@@ -80,38 +50,70 @@ def normalTrace():
             lost = True
             losted = utime.ticks_ms()
 
-def kstCheck():
-    global kst_lft,kst_rig,kst_tik
+def kstCheck_T():
+    global kst_lft,kst_rig,kst_tik,skwww
     kst_lft -= 1
     kst_rig -= 1
     kst_tik -= 1
-    if mcp.read_adc(0)<shikii_line:
+    # d1 = [mcp.read_adc(1) > shikii_line, mcp.read_adc(2) > shikii_line, mcp.read_adc(3) > shikii_line]
+    # d2 = [mcp.read_adc(0) > shikii_line, mcp.read_adc(1) > shikii_line, mcp.read_adc(2) > shikii_line]
+    d1,d2 = [],[]
+    if mcp.read_adc(0)<shikii_line and all(d1):
         kst_lft = kst
-    if mcp.read_adc(3)<shikii_line:
+    if mcp.read_adc(3)<shikii_line and all(d2):
         kst_rig = kst
     if kst_lft > 0 and kst_rig > 0 and kst_tik < 1:
+        skwww = "T"
         return True
     return False
-    
+
 def kstMove():
     global kst_rig,kst_lft,kst_tik,losted
     o.move(0,kstzzny,10000,0)
-    sleep_ms(kszzntime)
+    utime.sleep_ms(kszzntime)
     o.move(0.1,ksty*kstmv,10000,kstsnk*kstmv)
-    sleep_ms(kstime)
+    utime.sleep_ms(kstime)
     kst_rig,kst_lft,losted=0,0,utime.ticks_ms()
     kst_tik = 600
         
+    
+def kstCheck_skw():
+    global kst_lft,kst_rig,kst_tik
+    if not lost:
+        if mcp.read_adc(0)<(shikii_line+50):
+            return "lft"
+        if mcp.read_adc(3)<(shikii_line+50):
+            return "rig"
+        print("NO SKW")
+    return False
+
+def kstMove_skw(dir:str):
+    global kst_rig,kst_lft,kst_tik,losted,skwww,skwtime
+    o.move(0,kstzzny,10000,0)
+    utime.sleep_ms(kszzntime)
+    if dir == "lft":
+        o.move(0.1,ksty*kstmv,10000,kstsnk*-1)
+    else:
+        o.move(0.1,ksty*kstmv,10000,kstsnk*1)
+    utime.sleep_ms(kstime)
+    kst_rig,kst_lft,losted=0,0,utime.ticks_ms()
+    kst_tik = 600
+    skwtime = 0
+    skwww = ""
+    
 def sideTrace():
     global lost,latestmove
-    if mcp.read_adc(3) < shikii_line:
-        o.move(yoko,mae*0.5,spd,senkai*1.5)
-        lost = False
-        latestmove = "l"
-    elif mcp.read_adc(0) < shikii_line:
-        o.move(-yoko,mae*0.5,spd,-senkai*1.5)
-        lost = False
-        latestmove = "r"
+    if lost and skwtime == 0:
+        if mcp.read_adc(3) < shikii_line:
+            o.move(yoko,mae*0.5,spd,senkai*2)
+            while mcp.read_adc(2) < (shikii_line):pass
+            lost = False
+            latestmove = "l"
+        elif mcp.read_adc(0) < shikii_line:
+            o.move(-yoko,mae*0.5,spd,-senkai*2)
+            while mcp.read_adc(1) < (shikii_line):pass
+            lost = False
+            latestmove = "r"
 
 def lostCheck():
     global losted, lost
@@ -135,6 +137,38 @@ def distCheck():
     dist = hcsr.distance()
     return a
 
+o = omni.Omni(PWM_LIST,IN_2_LIST,IN_1_LIST,True)
+shikii_dis = 10
+dist = 0
+shikii_line = 150
+spd = 25000
+senkai = 1
+mae = 1
+yoko = 0
+check = False
+waitime = 500
+kst_rig = 0
+kst_lft = 0
+kstmv = -1 #交差点方向 -1左　1右
+kstsnk = 1.3 #交差点旋回速度
+ksty = 0.5 #交差点Vy値
+kstime = 750 #交差点曲がり時間
+kstzzny = 1 #交差点事前前進Vy値
+kszzntime = 700 #交差点事前前進時間
+kst = 200 #交差点判定のはば
+kst_tik = 50 #交差点後の待機
+skw = 1 #ト字の検知のはば
+skwwait = 100 #ト字検知後のT字待機
+skw_waitime = 0
+skwtime = 0
+skwww = ""
+lost = False
+lostime = 500
+losted = 0
+latest = None
+latestmove = None
+hcsr.activate()
+
 try:
     while True:
         if rp2.bootsel_button() == 1:
@@ -144,23 +178,59 @@ try:
         # p(mcp.read_adc(2), 200, " s3\n")
         # p(mcp.read_adc(3), 200, " s4\n\n\n\n")
 
-        if kstCheck():
-            print("INTERSECT DETECTED")
-            kstMove()
+        
         
         if lostCheck():
             print("LINE LOSTED")
             o.stop()
             break
-        else:print("LINE DETECTED")
 
         if distCheck():
-            print(f"TOO CLOSE {dist:.2f}cm")
-            break
-        else:print("FULLY")
-        normalTrace()
+            o.stop()
+            utime.sleep_ms(100)
+            print(f"too close? {dist:.2f}cm")
+            if distCheck():
+                print(f"TOO CLOSE {dist:.2f}cm")
+                break
+        else:pass
+        
+        if kstCheck_T():
+            print("T INTERSECT",latestmove)
+            kstMove()
+        
+        normalTrace()        
+
+
+        if skw_waitime == 0:
+            skwww = kstCheck_skw()
+            if skwww:
+                if skwww == "lft":
+                    print("LEFT INTERSECT?")
+                    skwtime -= 1
+                else:
+                    print("RIGHT INTERSECT?")
+                    skwtime += 1
+            else:skwtime = 0
+        
+            if skwtime >= skw:
+                print("RIGHT")
+                skw_waitime = skwwait
+                kstMove_skw("rig")
+            elif skwtime <= -skw:
+                print("LEFT")
+                skw_waitime = skwwait
+                kstMove_skw("lft")
+        elif skw_waitime == 1:
+            if not skwww == "T":
+                print(f"SKW {skwww}")
+        else:
+            skw_waitime -= 1
+        if skwww:
+            print(skwww)
+
         sideTrace()
-        # utime.sleep_ms(5) # ループが早すぎないように少しウェイトを入れる
+        
+        # utime.sleep_ms(5)
 except Exception as e:
     print("\nError:", e)
 finally:
--- mcp3008.py
+++ mcp3008.py
@@ -4,11 +4,17 @@ class mcp:
         self.spi = spi
         self.cs = cs
         cs.on()
-
-    def read_adc(self,channel=0):
-        # 送信データの作成 (スタートビット、チャンネル指定、ダミー)
+        print("\033[1m"+"\033[33m"+"="*23+"MCP3008"+"="*20+"\033[0m")
+    def read_adc(self, channel=0):
+        # 1バイト目: 0x01 (Start bit)
+        # 2バイト目: (8 + channel) << 4
+        #   -> 8 (10002) = SGL=1, D2=0, D1=0, D0=0 
+        #   -> << 4 で上位4ビット(MSB側)へ配置: [SGL][D2][D1][D0][0][0][0][0]
+        self.cs.off()
         tx = bytes([0x01, (8 + channel) << 4, 0x00])
         rx = bytearray(3)
         self.spi.write_readinto(tx, rx)
-        print(rx)
+        self.cs.on()
+        # 10ビットデータの復元
+        # rx[1] の下位2ビット（D9, D8） + rx[2]（D7〜D0）
         return ((rx[1] & 0x03) << 8) | rx[2]
